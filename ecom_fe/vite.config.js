5f5b6ee24c27ffeb71cdac9174b349b4d9889568
durden <hlib.ivanov@innowise.com>
2025-07-24 12:14:00 +0200

    docker settings

--- ecom_fe/vite.config.js
+++ ecom_fe/vite.config.js
@@ -2,13 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+  },
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
